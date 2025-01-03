6f9beba526958e1d0fc365b7b0bf229e5841c5ae
bsosnowicz <bartek.sosnowicz@gmail.com>
2025-01-04 00:55:31 +0100

    navbar dla admin oraz user

---
+++ Test/Views/Home/Asortyment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test.Views.Home
+{
+    public class AsortymentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
