1f6788188214bd1688df83a1c1a047cbf492bde2
sonaxik <dawid.szarlej@op.pl>
2025-01-03 04:19:29 +0100

    Nowy uzytkownik dostaje role User

--- Test/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Test/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -136,6 +136,7 @@ namespace Test.Areas.Identity.Pages.Account
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    await _userManager.AddToRoleAsync(user, "User");
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
